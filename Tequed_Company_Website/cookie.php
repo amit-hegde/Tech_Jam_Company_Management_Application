e8acecff88207f7cb29ec6bb0783a190c543a7b7
Amit U Hegde <2ke18cs005@kleit.ac.in>
2022-07-28 20:47:24 +0530

    Delete Tequed_Company_Website directory

--- Tequed_Company_Website/cookie.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// setcookie("product","Dell", time() + (86400 * 2), "/");
-setcookie("product","ASUS", time() + (86400 * 2), "/");
-
-?>
--- Tequed_Company_Website/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("isauth","", time() - 3600, "/");
-setcookie("admin_name","",time() - 3600, "/");
-header("location:logadmin")
-?>
